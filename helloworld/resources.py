79d56cfdb3b96e8edbdfc63ee311cc9b67a9c425
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2016-01-29 19:26:25 +0000

    simple create object

--- helloworld/resources.py
+++ helloworld/resources.py
@@ -1,10 +1,24 @@
+import flask
 from flask_restful import Resource
+from werkzeug.exceptions import BadRequest
 import helloworld.models
+from helloworld.database import db
 
 
 class PlanetRes(Resource):
     def post(self):
-        return
+        data = flask.request.get_json()
+        if not data:
+            raise BadRequest("Request data must be JSON.")
+        try:
+            name = data['name']
+        except KeyError:
+            raise BadRequest("JSON body must include the `name` key.")
+
+        planet = helloworld.models.Planet(name=name)
+        db.session.add(planet)
+        db.session.commit()
+        return '', 202  # Accepted
 
     def get(self):
         return [{'id': planet.id, 'name': planet.name}
